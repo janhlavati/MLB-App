6902248f7de50fa9ed6cfff182e5143aa003e91c
janhlavati <janhlavaty11@gmail.com>
2026-01-06 19:15:07 +0100

    Initial commit

---
+++ src/main/java/com/mlb_app/MLB_App/MlbAppApplication.java
@@ -0,0 +1,13 @@
+package com.mlb_app.MLB_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlb_app/MLB_App/MlbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb_app.MLB_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
