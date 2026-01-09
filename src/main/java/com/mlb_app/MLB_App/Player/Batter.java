c0a7c5f3934a7739690d6ab2b0bc57d42df11fd8
janhlavati <janhlavaty11@gmail.com>
2026-01-09 01:18:59 +0100

    Edited the batter class

--- src/main/java/com/mlb_app/MLB_App/Player/Batter.java
+++ src/main/java/com/mlb_app/MLB_App/Player/Batter.java
@@ -3,8 +3,8 @@ package com.mlb_app.MLB_App.Player;
 public class Batter {
 
     private String name;
+    private int age;
     private String team;
-    private String nation;
-    private String position;
+    private String league;
 
 }
