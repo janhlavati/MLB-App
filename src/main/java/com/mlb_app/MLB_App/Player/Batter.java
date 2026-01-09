af636643d941b4a270ec505b75e960c36510be07
janhlavati <janhlavaty11@gmail.com>
2026-01-09 12:40:13 +0100

    Added all of the variables for Batter

--- src/main/java/com/mlb_app/MLB_App/Player/Batter.java
+++ src/main/java/com/mlb_app/MLB_App/Player/Batter.java
@@ -6,5 +6,34 @@ public class Batter {
     private int age;
     private String team;
     private String league;
+    private double war;
+    private int g;
+    private int pa;
+    private int ab;
+    private int r;
+    private int h;
+    private int doubles;
+    private int triples;
+    private int hr;
+    private int rbi;
+    private int sb;
+    private int cs;
+    private int bb;
+    private int so;
+    private double ba;
+    private double obp;
+    private double slg;
+    private double ops;
+    private double opsPlus;
+    private double rOba;
+    private int rBatPlus;
+    private int tb;
+    private int gidp;
+    private int hbp;
+    private int sh;
+    private int sf;
+    private int ibb;
+    private String pos;
+    private String awards;
 
 }
