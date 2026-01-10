6f8fb33d465e6e6b05e72f313dff8eeccf50d037
janhlavati <janhlavaty11@gmail.com>
2026-01-10 21:09:32 +0100

    Generated the constructors for Batter class

--- src/main/java/com/mlb_app/MLB_App/Player/Batter.java
+++ src/main/java/com/mlb_app/MLB_App/Player/Batter.java
@@ -36,4 +36,43 @@ public class Batter {
     private String pos;
     private String awards;
 
+    public Batter() {
+
+    }
+
+    public Batter(String name, int age, String team, String league, double war, int g, int pa, int ab, int r, int h, int doubles, int triples, int hr, int rbi, int sb, int cs, int bb, int so, double ba, double obp, double slg, double ops, double opsPlus, double rOba, int rBatPlus, int tb, int gidp, int hbp, int sh, int sf, int ibb, String pos, String awards) {
+        this.name = name;
+        this.age = age;
+        this.team = team;
+        this.league = league;
+        this.war = war;
+        this.g = g;
+        this.pa = pa;
+        this.ab = ab;
+        this.r = r;
+        this.h = h;
+        this.doubles = doubles;
+        this.triples = triples;
+        this.hr = hr;
+        this.rbi = rbi;
+        this.sb = sb;
+        this.cs = cs;
+        this.bb = bb;
+        this.so = so;
+        this.ba = ba;
+        this.obp = obp;
+        this.slg = slg;
+        this.ops = ops;
+        this.opsPlus = opsPlus;
+        this.rOba = rOba;
+        this.rBatPlus = rBatPlus;
+        this.tb = tb;
+        this.gidp = gidp;
+        this.hbp = hbp;
+        this.sh = sh;
+        this.sf = sf;
+        this.ibb = ibb;
+        this.pos = pos;
+        this.awards = awards;
+    }
 }
