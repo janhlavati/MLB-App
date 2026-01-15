17e3db3f0b9a63b71489fa2d901bf79bfa2cebe8
janhlavati <janhlavaty11@gmail.com>
2026-01-15 10:52:07 +0100

    Made the list of all team abbreviations

--- Python/main.py
+++ Python/main.py
@@ -3,3 +3,9 @@ import io
 import random
 from time import sleep
 from playwright.sync_api import sync_playwright
+
+#List of every team abbreviation
+teams = ["ARI", "ATL", "BAL", "BOS", "CHC", "CHW", "CIN", "CLE", "COL", "DET", "HOU", "KCR", "LAA",
+         "LAD", "MIA", "MIL", "MIN", "NYM", "NYY", "OAK", "PHI", "PIT", "SDP", "SFG", "SEA", "STL", "TBR",
+         "TEX", "TOR", "WSN"]
+
