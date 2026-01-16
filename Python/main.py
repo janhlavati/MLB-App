dadab07c1b1dfedf3adbe27962a4e687446c8a83
janhlavati <janhlavaty11@gmail.com>
2026-01-16 09:31:19 +0100

    Started the logic of web scraper

--- Python/main.py
+++ Python/main.py
@@ -9,3 +9,13 @@ teams = ["ARI", "ATL", "BAL", "BOS", "CHC", "CHW", "CIN", "CLE", "COL", "DET", "
          "LAD", "MIA", "MIL", "MIN", "NYM", "NYY", "OAK", "PHI", "PIT", "SDP", "SFG", "SEA", "STL", "TBR",
          "TEX", "TOR", "WSN"]
 
+def get_specific_team_urls(teams, year="2025"):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        )
+        page = context.new_page()
+
+        url = f"https://www.baseball-reference.com/teams/{teams}/{year}.shtml"
+        print(f"Scraping {teams} for {year}...")
