64651c1b4ca59c91572d2f2199f1a012e91b5908
janhlavati <janhlavaty11@gmail.com>
2026-01-25 12:58:44 +0100

    Added the teams component functionality

---
+++ Frontend/src/components/Teams/index.js
@@ -0,0 +1,61 @@
+const { use } = require("react");
+
+const Teams = () => {
+    const[letterClass, setLetterClass] = useState('text-animate');
+    const[searchQuery, setSearchQuery] = useState('');
+    const[filteredTeams, setFilteredTeams] = useState(teamsData);   
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    },);
+
+    useEffect(() => {   
+        const filtered = teamsData.filter(team =>
+            team.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        setFilteredTeams(filtered);
+    }, [searchQuery]);
+
+        const handleSearchChange = event => {
+            setSearchQuery(event.target.value);
+        }
+
+        const renderTeam = (teams) => {
+            return (
+                <div className="images-container">
+                    {teams.map((team, idx) => (
+                        <div key={idx} className="image-box">
+                            <img src={team.cover} alt="teams" className="team-image" />
+                            <div className="content">
+                                <p className="team-title">{team.title}</p>
+                                <Link className="btn" to={`/data?team=${encodeURIComponent(team.title)}`}>View Details</Link>  
+                              </div>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+    return (
+        <>
+        <div className="container teams-page">
+            <div className="search-bar">  
+                <input
+                    type="text"
+                    placeholder="Search teams..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                />
+            </div>
+            <div>{renderTeam(filteredTeams)}</div>
+        </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+export default Teams;
