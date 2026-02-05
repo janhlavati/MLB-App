74cd11d13f0348e921af7e14348a809d7bb5c23e
janhlavati <janhlavaty11@gmail.com>
2026-02-05 11:41:11 +0100

    Configured the logo

--- Frontend/src/components/Sidebar/index.js
+++ Frontend/src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom"
-import LogoPL from '../../assets/images/mlb.webp'
+import LogoPL from '../../assets/images/mlb.jpg'
 import { useState } from 'react'
 
 const Sidebar = () => {
