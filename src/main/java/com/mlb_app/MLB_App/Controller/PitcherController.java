f875e5f6ec1fd323c05d98a247b60847d1ba87cd
janhlavati <janhlavaty11@gmail.com>
2026-02-06 10:59:54 +0100

    Added the get method in pitcher controller class

--- src/main/java/com/mlb_app/MLB_App/Controller/PitcherController.java
+++ src/main/java/com/mlb_app/MLB_App/Controller/PitcherController.java
@@ -1,4 +1,39 @@
 package com.mlb_app.MLB_App.Controller;
 
+import com.mlb_app.MLB_App.Player.Pitcher;
+import com.mlb_app.MLB_App.Service.PitcherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5050")
+@RequestMapping(path = "api/v1/pitcher")
 public class PitcherController {
+    private final PitcherService pitcherService;
+
+    @Autowired
+    public PitcherController(PitcherService pitcherService) {
+        this.pitcherService = pitcherService;
+    }
+
+    @GetMapping
+    public List<Pitcher> getPitchers(
+            @RequestParam(required = false) String team,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String position) {
+                if(team != null && position != null) {
+                    return pitcherService.getPitchersByTeamAndPosition(team, position);
+                }else if(team != null) {
+                    return pitcherService.getPitchersByTeam(team);
+                }else if(name != null){
+                    return pitcherService.getPitchersByName(name);
+                }else if(position != null) {
+                    return pitcherService.getPitchersByPosition(position);
+                }else {
+                    return pitcherService.getPitchers();
+                }
+
+            }
 }
