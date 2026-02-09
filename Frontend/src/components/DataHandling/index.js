ed567176121cb3cb70824afb0f2affcfa8640d79
janhlavati <janhlavaty11@gmail.com>
2026-02-09 17:46:56 +0100

    Added functionalities of data handling component

--- Frontend/src/components/DataHandling/index.js
+++ Frontend/src/components/DataHandling/index.js
@@ -2,5 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const DataHandling = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [data, setPitcherData] = useState([]);
 
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const teamValue = params.get('team');
+
+        if(teamValue) {
+            axios.get(`http://localhost:8080/api/pitchers?team=${encodeURIComponent(teamValue)}`)
+            .then(response => {
+                setData(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setPitcherData([]);
+                setLoading(false);
+            });
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    
 }
