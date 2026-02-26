d8734835863f3663866316a2047472d722773d15
janhlavati <janhlavaty11@gmail.com>
2026-02-26 18:27:01 +0100

    Inititalized and imported teambatting scss file

--- Frontend/src/components/TeamBatting/index.js
+++ Frontend/src/components/TeamBatting/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
+import "./index.scss";
 import { Link } from 'react-router-dom';
 import teamData from "../../data/teamBatting.json";
 
