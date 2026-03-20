8c16f3f176ef39e88c2bf81ac46d4bf397b8e30d
janhlavati <janhlavaty11@gmail.com>
2026-03-20 08:14:45 +0100

    Background changes

--- Frontend/src/components/Home/index.js
+++ Frontend/src/components/Home/index.js
@@ -8,9 +8,9 @@ const Home = () => {
         <>
             <div className="container-home-page">
                 <div className="text-zone" style={{ textAlign: 'center' }}>
-                    <b><h1 style={{ display: 'block', border: '1px solid rgba(255, 255, 255, 0.3)', borderRadius: '20px', marginBottom: '20px', padding: '10px', fontSize: '48px', backgroundColor: '#4357ee',  }}>MLB Stats</h1></b>
-                    <Link to="/teamPitching" className="flat-button" style={{margin: '10px', fontSize: '18px', font: 'caption', color: '#ffffffff' }}>Pitchers</Link>
-                    <Link to="/teamBatting" className="flat-button" style={{margin: '10px', fontSize: '18px', font: 'caption', color: '#ffffffff' }}>Batters</Link>
+                    <b><h1 style={{ display: 'block', marginBottom: '20px', padding: '10px', fontSize: '48px'}}>MLB Stats</h1></b>
+                    <Link to="/teamPitching" className="flat-button" style={{margin: '40px', fontSize: '18px', font: 'caption', color: '#ffffffff' }}>Pitchers</Link>
+                    <Link to="/teamBatting" className="flat-button" style={{margin: '40px', fontSize: '18px', font: 'caption', color: '#ffffffff' }}>Batters</Link>
                 </div>
             </div>
             <Loadert type="pacman" />
